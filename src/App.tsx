217bc1cac01bddd30bb87727a57801305199bfbd
Jürmo Mihhailov <jurmoharak@gmail.com>
2023-11-21 13:28:55 +0200

    Update weather city to London

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WeatherData from './components/WearherData';
-import InputNewCity from './components/InputNewCity';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <WeatherData city="Tartu"/>
+      <WeatherData city="London"/>
     </div>
   );
 }
--- src/components/WearherData.tsx
+++ src/components/WearherData.tsx
@@ -1,61 +1,65 @@
-import { on } from "events";
 import React, { useEffect, useState } from "react";
 import InputNewCity from "./InputNewCity";
 
 const WearherDataPromise = (city: string) => {
-    const api_key = "e3d20738707b74842b79cab7c1bd5ce7"
-    const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+  const api_key = "e3d20738707b74842b79cab7c1bd5ce7"
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
 
-    return new Promise((resolve, reject) => {
-      fetch(api)
-        .then(response => {
-          return response.json()
+  return new Promise((resolve, reject) => {
+    fetch(api)
+      .then(response => {
+        return response.json()
 
-        })
-        .then(data => {
-          let description = data.weather[0].description;
-          let city = data.name;
-          let temp = Math.round(parseFloat(data.main.temp));
-          let result = { description: description, city: city, temp: temp, error: null };
-          resolve(result);
-        })
-        .catch(error => {
-          reject(error);
-        })
-    })
-  }
+      })
+      .then(data => {
+        let description = data.weather[0].description;
+        let city = data.name;
+        let temp = Math.round(parseFloat(data.main.temp));
+        let result = { description: description, city: city, temp: temp, error: null };
+        resolve(result);
+      })
+      .catch(error => {
+        reject(error);
+      })
+  })
+}
 
 const WeatherData = (props: any) => {
-    const [weather, setWeather] = useState({description: "", city: "", temp: 0})
-    const [city, setCity] = useState("");
-    useEffect(() => { 
-      let place = props.city;
-      if (city !== "") {
-        place = city;
-      }
-        WearherDataPromise(place) 
-        .then((data: any) => {
+  const [weather, setWeather] = useState({ description: "", city: "", temp: 0 })
+  const [city, setCity] = useState("");
 
-            setWeather({description: data.description, city: data.city, temp: data.temp - 273})
-        })
-        .catch((error: any) => {
-            alert('City not found');
-            console.log(error)
-        })
-    }, [city])
-
-    const onAddCityCallback = (city_str: string) => {
-      setCity(city_str);
+  useEffect(() => {
+    let place = JSON.parse(localStorage.getItem('City') || '{}');
+    console.log(place);
+    if (Object.keys(place).length === 0) {
+      localStorage.setItem('City', JSON.stringify("London"));
     }
+    if (city !== "") {
+      place = city;
+    }
+    WearherDataPromise(place)
+      .then((data: any) => {
+        localStorage.setItem('City', JSON.stringify(place));
+        setWeather({ description: data.description, city: data.city, temp: data.temp - 273 })
+      })
+      .catch((error: any) => {
+        alert('City not found');
+        console.log(error)
+      })
+  }, [city])
 
-    return(
-        <div>
-            <InputNewCity onAddCity={onAddCityCallback}></InputNewCity>
-            <h2>{weather.city}</h2>
-            <h3>{weather.description}</h3>
-            <h3>{weather.temp} °C</h3>
-        </div>
-    )
+  const onAddCityCallback = (city_str: string) => {
+    setCity(city_str);
+  }
+
+  return (
+    <div>
+      <InputNewCity onAddCity={onAddCityCallback}></InputNewCity>
+      <h2>{weather.city}</h2>
+      <h3>{weather.description}</h3>
+      <h3>{weather.temp} °C</h3>
+    </div>
+  )
 }
 
 export default WeatherData
